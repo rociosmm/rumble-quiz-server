74024500222a6493a56e5d8c1726aab2112f10f3
Alex <alexdtaylor93@gmail.com>
2024-07-01 11:25:52 +0100

    set up index file for test data

---
+++ db/data/test-data/index.js
@@ -0,0 +1,7 @@
+exports.avatars = require("./avatars");
+exports.colourThemes = require("./colour-themes");
+exports.friendship = require("./friendship");
+exports.logs = require("./logs");
+exports.notifications = require("./notifications");
+exports.sounds = require("./sounds");
+exports.users = require("./users");
