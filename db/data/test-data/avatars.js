4ecbcaf0eb1508a5da1b2833698824c248f5349e
Alex <alexdtaylor93@gmail.com>
2024-07-01 10:44:31 +0100

    added some test data for avatars, logs and notifications

---
+++ db/data/test-data/avatars.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    avatar_id: 1,
+    avatar_url:
+      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+  },
+  {
+    avatar_id: 2,
+    avatar_url:
+      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=600",
+  },
+  {
+    avatar_id: 3,
+    avatar_url:
+      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=500",
+  },
+];
