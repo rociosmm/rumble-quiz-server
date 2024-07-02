63b16240a951d7a8c21deb0e009d537ccfa6db63
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-07-02 16:59:34 +0100

    Added GET request for single avatars using avatar ID

--- __tests__/avatars.tests.js
+++ __tests__/avatars.tests.js
@@ -8,18 +8,49 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("/api/avatars", () => {
-    test('GET 200: responds with a list of avatars', () => {
-        return request(app)
-        .get("/api/avatars")
-        .expect(200)
-        .then(({body}) => {
-            body.avatars.forEach((avatar) => {
-                expect(avatar).toMatchObject({
-                    avatar_id: expect.any(Number),
-                    avatar_name: expect.any(String),
-                    avatar_url: expect.any(String)
-                })
-            })
-        })
-    });
+  test("GET 200: responds with a list of avatars", () => {
+    return request(app)
+      .get("/api/avatars")
+      .expect(200)
+      .then(({ body }) => {
+        body.avatars.forEach((avatar) => {
+          expect(avatar).toMatchObject({
+            avatar_id: expect.any(Number),
+            avatar_name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
+describe("/api/avatars/:avatar_id", () => {
+  test("GET 200: responds with a single avatar using avatar ID", () => {
+    return request(app)
+      .get("/api/avatars/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.avatar).toMatchObject({
+          avatar_id: expect.any(Number),
+          avatar_name: expect.any(String),
+          avatar_url: expect.any(String),
+        });
+      });
+  });
+  test("GET 404: responds with Avatar Not Found error message when passed avatar ID that does not exist", () => {
+    return request(app)
+      .get("/api/avatars/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Avatar Not Found");
+      });
+  });
+  test("GET 400: responds with Bad Request error message when passed an invalid avatar ID", () => {
+    return request(app)
+      .get("/api/avatars/hola")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
 });
--- models/avatars.model.js
+++ models/avatars.model.js
@@ -3,9 +3,21 @@ const db = require("../db/connection");
 exports.fetchAvatars = () => {
   return db.query("SELECT * FROM avatars;").then(({ rows }) => {
     if (!rows.length) {
-      return Promise.reject({ status: 404, msg: "No avatars found" });
+      return Promise.reject({ status: 404, msg: "No Avatars Found" });
     } else {
       return rows;
     }
   });
+};
+
+exports.fetchAvatarById = (avatar_id) => {
+  return db
+    .query(`SELECT * FROM avatars WHERE avatar_id = $1;`, [avatar_id])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Avatar Not Found" });
+      } else {
+        return rows[0];
+      }
+    });
 };
