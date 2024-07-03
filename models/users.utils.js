5e7a4974fed756de5dca5edf522a5fbb24438f6b
jowatson2011 <jowatson@protonmail.com>
2024-07-03 14:51:50 +0100

    Removed leftover console.logs

--- models/users.utils.js
+++ models/users.utils.js
@@ -2,14 +2,12 @@ const db = require("../db/connection");
 const format = require("pg-format");
 
 exports.checkIfExists = (propertyName, value) => {
-  console.log("utils");
   const queryStr = format(
     `SELECT * FROM users
         WHERE %I = $1;`,
     propertyName
   );
   return db.query(queryStr, [value]).then(({ rows }) => {
-    console.log(rows);
     if (rows.length > 1) {
       return Promise.reject({
         status: 400,
