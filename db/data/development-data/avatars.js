2a4a20b16a5adf7cab67197fe81a47901d232733
jowatson2011 <jowatson@protonmail.com>
2024-07-03 15:08:05 +0100

    Added development data

--- db/data/development-data/avatars.js
+++ db/data/development-data/avatars.js
@@ -1,14 +1,42 @@
 module.exports = [
   {
-    avatar_name: "cat",
-    avatar_url: "https://github.com/JoWatson2011/rumble-quiz-server",
+    avatar_name: "Lion",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
   },
   {
-    avatar_name: "dog",
-    avatar_url: "https://github.com/JoWatson2011/rumble-quiz-server",
+    avatar_name: "Elephant",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
   },
   {
-    avatar_name: "rabbit",
-    avatar_url: "https://github.com/JoWatson2011/rumble-quiz-server",
+    avatar_name: "Giraffe",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Zebra",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Tiger",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Panda",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Kangaroo",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Penguin",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Koala",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
+  },
+  {
+    avatar_name: "Dolphin",
+    avatar_url: "https://img.icons8.com/?size=100&id=823&format=png",
   },
 ];
