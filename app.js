7c8ed1991558ed28e17a1321f8552c49f1775f0f
jowatson2011 <jowatson@protonmail.com>
2024-07-04 16:34:05 +0100

    Added / as a route in app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const {
 } = require("./routes/index.js");
 const app = express();
 app.use(express.json());
-
+app.use("/", apiRouter);
 app.use("/api", apiRouter);
 
 app.use("/api/users", usersRouter);
