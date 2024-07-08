a84f4467cbcacef9ff36f0e5cc24a12ef5615cc0
Alex <alexdtaylor93@gmail.com>
2024-07-08 09:11:51 +0100

    added query onto the end of each avatar url

--- db/data/development-data/avatars.js
+++ db/data/development-data/avatars.js
@@ -2,51 +2,51 @@ module.exports = [
   {
     avatar_name: "Bear",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-bear-48.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-bear-48.png?raw=true",
   },
   {
     avatar_name: "Elephant",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-elephant-64.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-elephant-64.png?raw=true",
   },
   {
     avatar_name: "Gazelle",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-gazelle-64.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-gazelle-64.png?raw=true",
   },
   {
     avatar_name: "Giraffe",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-giraffe-48.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-giraffe-48.png?raw=true",
   },
   {
     avatar_name: "Gorilla",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-gorilla-48.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-gorilla-48.png?raw=true",
   },
   {
     avatar_name: "Hippo",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-hippo-64.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-hippo-64.png?raw=true",
   },
   {
     avatar_name: "Leopard",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-leopard-48.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-leopard-48.png?raw=true",
   },
   {
     avatar_name: "Lion",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-lion-48.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-lion-48.png?raw=true",
   },
   {
     avatar_name: "Rhino",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-rhinoceros-64.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-rhinoceros-64.png?raw=true",
   },
   {
     avatar_name: "Tiger",
     avatar_url:
-      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-tiger-48.png",
+      "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-tiger-48.png?raw=true",
   },
 ];
