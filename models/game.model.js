d35d009feb8b901e61f09b3152b1c1b7533784dc
jowatson2011 <jowatson@protonmail.com>
2024-07-10 10:56:09 +0100

    Avatar is no longer updated in server when player is eliminated

--- models/game.model.js
+++ models/game.model.js
@@ -25,8 +25,6 @@ const updateGameData = (topic_id, answerData) => {
     const index = game.players_active.indexOf(answerData.username);
     game.players_active.splice(index, 1);
     game.players_eliminated.push(answerData.username);
-    game.avatar_urls[answerData.username] =
-      "https://img.icons8.com/?size=100&id=63688&format=png&color=000000";
   }
   game.points[answerData.username] += answerData.points;
 };
