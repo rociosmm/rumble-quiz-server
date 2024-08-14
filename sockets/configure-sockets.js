ae7aecc16e2241f549de996f1b70b4c13ea8ea32
rociosmm <rociosmm@yahoo.com>
2024-08-14 15:07:29 +0100

    Change the room size to 2 players

--- sockets/configure-sockets.js
+++ sockets/configure-sockets.js
@@ -12,7 +12,7 @@ const openTdb_url = axios.create({
   baseURL: "https://opentdb.com",
 });
 
-exports.configureSockets = (server, ROOM_LIMIT = 3) => {
+exports.configureSockets = (server, ROOM_LIMIT = 2) => {
   const io = socketIO(server, {
     cors: {
       origin: "*",
